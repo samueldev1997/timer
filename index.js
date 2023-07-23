0767a7661fff052a9a375f669da5ec02c618bf87
Samuel Jonas <139514932+samueldev1997@users.noreply.github.com>
2023-07-23 19:47:16 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    })
+}
+
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+let segundos = 0;
+let timer;
+
+function iniciaRelogio() {
+    timer = setInterval(function () {
+        segundos++;
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000)
+}
+
+document.addEventListener('click', function (event) {
+    const elementos = event.target;
+
+    if (elementos.classList.contains('iniciar')) {
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        iniciaRelogio();
+    }
+
+    if (elementos.classList.contains('pausar')) {
+        relogio.classList.add('pausado');
+        clearInterval(timer);
+
+    }
+
+    if (elementos.classList.contains('zerar')) {
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        relogio.innerHTML = '00:00:00';
+        segundos = 0;
+
+    }
+})
+
+
